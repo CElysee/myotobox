763e315f2e0d40e04d79617397b606fcbb6f427b
Elysee CONFIANCE <ccelyse1@gmail.com>
2024-05-15 10:37:02 +0200

    added loading on selling cars component v3

--- src/frontend/components/brandsWithBodyType/BrandsWithBodyType.jsx
+++ src/frontend/components/brandsWithBodyType/BrandsWithBodyType.jsx
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from "react";
 import axiosInstance from "../../../../utils/AxiosInstance";
 import { Link } from "react-router-dom";
+import ContentLoader from "react-content-loader";
 
 function BrandsWithBodyType() {
+  const [loading, setLoading] = useState(true);
   const [carBodyTypeList, setCarBodyTypeList] = useState([]);
+  const [contentLaoderCount, setContentLoaderCount] = useState([1, 2, 3, 4, 5]);
   const imageBaseUrl = import.meta.env.VITE_REACT_APP_API;
   useEffect(() => {
     const fetchData = async () => {
       const response = await axiosInstance.get("/car_body_type/list");
       setCarBodyTypeList(response.data.car_body);
+      setLoading(false);
     };
     fetchData();
   }, []);
@@ -88,30 +92,47 @@ function BrandsWithBodyType() {
               </Link>
             </li>
           </ul>
-          <div className="tab-content mt-5">
-            <div className="ct-row d-flex align-items-center justify-content-center flex-wrap">
-              {carBodyTypeList.length > 0
-                ? carBodyTypeList.map((carBodyType, index) => (
-                    <Link
-                      className="ct-col mobile-body-shape"
-                      key={index}
-                      to={`/bodyShape/${carBodyType.body_type_name}`}
-                    >
-                      <div className="brand-card text-center bg-white position-relative rounded">
-                        <img
-                          src={`${imageBaseUrl}/BodyTypeImage/${carBodyType.body_type_image}`}
-                          className="img-fluid icon-black"
-                          alt={carBodyType.body_type_name}
-                        ></img>
-                        <h5 className="mt-3 mb-0">
-                          {carBodyType.body_type_name}
-                        </h5>
-                      </div>
-                    </Link>
-                  ))
-                : null}
+
+          {loading ? (
+            contentLaoderCount.map((item, index) => (
+              <ContentLoader
+                style={{ width: "20%", height: "150", padding: "10px", marginTop: "15px"}}
+                speed={1}
+                backgroundColor="#eee"
+                foregroundColor="#e8e7e7"
+                key={index}
+              >
+                <rect x="2" y="4" rx="8" ry="8" width="70" height="20" />
+                <rect x="100" y="4" rx="8" ry="8" width="60" height="20" />
+                <rect x="0" y="40" rx="5" ry="5" width="650" height="415" />
+              </ContentLoader>
+            ))
+          ) : (
+            <div className="tab-content mt-5">
+              <div className="ct-row d-flex align-items-center justify-content-center flex-wrap">
+                {carBodyTypeList.length > 0
+                  ? carBodyTypeList.map((carBodyType, index) => (
+                      <Link
+                        className="ct-col mobile-body-shape"
+                        key={index}
+                        to={`/bodyShape/${carBodyType.body_type_name}`}
+                      >
+                        <div className="brand-card text-center bg-white position-relative rounded">
+                          <img
+                            src={`${imageBaseUrl}/BodyTypeImage/${carBodyType.body_type_image}`}
+                            className="img-fluid icon-black"
+                            alt={carBodyType.body_type_name}
+                          ></img>
+                          <h5 className="mt-3 mb-0">
+                            {carBodyType.body_type_name}
+                          </h5>
+                        </div>
+                      </Link>
+                    ))
+                  : null}
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </section>
     </>
