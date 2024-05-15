2133341d14747b5440266288f5367313a9842aaa
Elysee CONFIANCE <ccelyse1@gmail.com>
2024-05-15 08:09:05 +0200

    added src with proper name

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./frontend/App.jsx";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "./store/store";
+import { store, persistor } from "./store/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
