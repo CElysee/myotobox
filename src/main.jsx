90d08cab1f6d9003a67a8c4f95f5f05ea37f8802
Elysee CONFIANCE <ccelyse1@gmail.com>
2024-07-22 21:55:50 +0200

    added seo to all pages v3

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import App from "./frontend/App.jsx";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./store/Store.js";
+import { HelmetProvider } from "react-helmet-async";
 
 // ReactDOM.createRoot(document.getElementById("root")).render(
 //   <React.StrictMode>
@@ -26,14 +27,15 @@ const APP = (
     <BrowserRouter>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <App />
+          <HelmetProvider>
+            <App />
+          </HelmetProvider>
         </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
 
- 
 const rootElement = document.getElementById("root");
 if (rootElement.hasChildNodes()) {
   hydrate(APP, rootElement);
