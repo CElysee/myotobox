28ea3705120ecb75fca4195e8f1ab7cbf2c91f1b
Elysee CONFIANCE <ccelyse1@gmail.com>
2024-07-22 20:25:41 +0200

    Added new seo react

--- src/frontend/pages/home/Home.jsx
+++ src/frontend/pages/home/Home.jsx
@@ -4,25 +4,40 @@ import RentCarsGrid from "../../components/rentCarsGrid/RentCarsGrid";
 import BrandsWithBodyType from "../../components/brandsWithBodyType/BrandsWithBodyType";
 import WhyChooseUs from "../../components/whyChooseUs/WhyChooseUs";
 import { Link } from "react-router-dom";
+import SEO from "../../components/SEO/SEO"
 
 function Home() {
   return (
-    <div className="main_content" style={{paddingTop: "70px"}}>
-      <BrandsWithBodyType />
-      <SellCarsGrid />
-      <div className="container mobile-gap">
-        <div className="row flex-nowrap">
-          <div className="d-md-flex col-md-12" style={{ justifyContent: "center" }}>
-            <Link className="btn btn-lg btn-primary view_more" to={"/buy_cars"}>
-              View more
-            </Link>
+    <>
+      <SEO
+        title="Easy Car Buying & Renting in Rwanda | Myotobox"
+        description="Buying or renting a car in Rwanda is easy with Myotobox. Choose from a wide selection of new and used cars at unbeatable rental rates!"
+        url="https://www.myotobox.rw/"
+        image="/assets/images/meta_image1.jpeg"
+      />
+      <div className="main_content" style={{ paddingTop: "70px" }}>
+        <BrandsWithBodyType />
+        <SellCarsGrid />
+        <div className="container mobile-gap">
+          <div className="row flex-nowrap">
+            <div
+              className="d-md-flex col-md-12"
+              style={{ justifyContent: "center" }}
+            >
+              <Link
+                className="btn btn-lg btn-primary view_more"
+                to={"/buy_cars"}
+              >
+                View more
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
 
-      <RentCarsGrid />
-      <WhyChooseUs />
-    </div>
+        <RentCarsGrid />
+        <WhyChooseUs />
+      </div>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,27 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+// import ReactDOM from "react-dom/client";
 // import App from './frontend/FrontendApp.jsx'
 // import "./index.css";
+import { hydrate, render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./frontend/App.jsx";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./store/Store.js";
-import { HelmetProvider } from "react-helmet-async";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <Provider store={store}>
+//         <PersistGate loading={null} persistor={persistor}>
+//           <App />
+//         </PersistGate>
+//       </Provider>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+const APP = (
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
@@ -19,4 +31,12 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
-);
+)
+
+ 
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(APP, rootElement);
+} else {
+  render(APP, rootElement);
+}
