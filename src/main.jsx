ac97b708cca0a2856cb159d279bd7577acdab300
Elysee CONFIANCE <ccelyse1@gmail.com>
2024-07-25 14:18:08 +0200

    trying helmet v1

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from './frontend/FrontendApp.jsx'
-// import "./index.css";
 import { hydrate, render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./frontend/App.jsx";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./store/Store.js";
-import { HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-        <HelmetProvider>
-          <App />
+          <HelmetProvider>
+            <App />
           </HelmetProvider>
         </PersistGate>
       </Provider>
